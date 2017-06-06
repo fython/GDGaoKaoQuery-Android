373f942e61e9206eb66a57aaeb1d15a951ce186f
Fung <fython@163.com>
2017-06-06 10:18:56 +0800

    可选 API
    Signed-off-by: Fung <fython@163.com>

--- app/src/main/kotlin/moe/feng/gd/gaokaoquery/api/CommonApi.kt
+++ app/src/main/kotlin/moe/feng/gd/gaokaoquery/api/CommonApi.kt
@@ -2,6 +2,7 @@ package moe.feng.gd.gaokaoquery.api
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import org.apache.http.HttpResponse
 import org.apache.http.client.CookieStore
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.DefaultHttpClient
@@ -18,28 +19,29 @@ object CommonApi {
 			"2016gklq" to "http://page-resoures.5184.com/cjquery/w/index.html?20160800gk_lq"
 	)
 
-	fun getCaptchaNewApi(referer: String?, random : Double = Random().nextDouble()): Bitmap?
-			= HttpGet("http://query-score.5184.com/web/captcha?random=$random")
-			.let {
-				it.addHeader("Referer", referer)
-				it.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" +
-						" AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-				it.addHeader("Host", "query-score.5184.com")
-				it
-			}
-			.let { client.execute(it) }
-			.let { saveCookie(client.cookieStore); it.entity }
-			.let { EntityUtils.toByteArray(it) }
-			.let {
-				try {
-					BitmapFactory.decodeByteArray(it, 0, it.size)
-				} catch (e: Exception) {
-					null
-				}
-			}
+	fun getCaptchaNewApi(referer: String?, random : Double = Random().nextDouble()): Bitmap? {
+		val request = HttpGet("http://query-score.5184.com/web/captcha?random=$random")
+		request.addHeader("Referer", referer)
+		request.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" +
+				" AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+		request.addHeader("Host", "query-score.5184.com")
+		val result : HttpResponse
+		try {
+			result = client.execute(request)
+		} catch (e: Exception) {
+			return null
+		}
+		saveCookie(client.cookieStore)
+		val bytes = EntityUtils.toByteArray(result.entity)
+		try {
+			return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+		} catch (e: Exception) {
+			return null
+		}
+	}
 
-	fun saveCookie(cs: CookieStore) {
-		cookie = cs.cookies.map {"${it.name}=${it.value}"}.reduce { total, next -> "$total; $next" }
+	fun saveCookie(cs: CookieStore?) {
+		cs?.cookies?.map {"${it.name}=${it.value}"}?.reduce { total, next -> "$total; $next" }?.let { cookie = it }
 	}
 
 }
--- app/src/main/kotlin/moe/feng/gd/gaokaoquery/ui/MainActivity.kt
+++ app/src/main/kotlin/moe/feng/gd/gaokaoquery/ui/MainActivity.kt
@@ -3,33 +3,42 @@ package moe.feng.gd.gaokaoquery.ui
 import android.app.AlertDialog
 import android.graphics.Color
 import android.os.Bundle
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import com.google.gson.Gson
+import android.view.View
+import android.widget.*
 import moe.feng.gd.gaokaoquery.R
 import moe.feng.gd.gaokaoquery.api.CommonApi
 import moe.feng.gd.gaokaoquery.api.QueryApi
+import moe.feng.gd.gaokaoquery.model.AdmissionResult
 import moe.feng.gd.gaokaoquery.model.ScoreResult
 import moe.feng.kotlinyan.common.AndroidExtensions
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.find
+import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
-import org.jetbrains.anko.toast
-import org.jetbrains.anko.uiThread
 
-class MainActivity : AppCompatActivity(), AndroidExtensions {
+class MainActivity : AppCompatActivity(), AndroidExtensions, AdapterView.OnItemSelectedListener {
 
 	val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
+	val spinner by lazy { find<Spinner>(R.id.spinner_year) }
 	val captchaImage by lazy { find<ImageView>(R.id.captcha_image) }
 	val queryScoreBtn by lazy { find<Button>(R.id.btn_query_score) }
 	val numberEdit by lazy { find<EditText>(R.id.edit_number) }
 	val birthEdit by lazy { find<EditText>(R.id.edit_birth) }
 	val captchaEdit by lazy { find<EditText>(R.id.edit_captcha) }
+	val rootLayout by lazy { find<CoordinatorLayout>(R.id.root_layout) }
+
+	val apiList = listOf(
+			mapOf("title" to "2016 高考成绩", "key" to "2016gkcj"),
+			mapOf("title" to "2016 高考录取", "key" to "2016gklq")
+	)
+
+	var currentApi = "2016gkcj"
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -38,22 +47,54 @@ class MainActivity : AppCompatActivity(), AndroidExtensions {
 
 		queryScoreBtn.onClick {
 			// TODO 检查输入框内容是否完整
-			doQueryScore()
+			if (currentApi.contains("gkcj")) {
+				doQueryScore()
+			} else if (currentApi.contains("gklq")) {
+				doQueryAdmission()
+			} else {
+				Snackbar.make(rootLayout, R.string.toast_unsupported_api, Snackbar.LENGTH_LONG).show()
+			}
 		}
 		captchaImage.onClick { refreshCaptcha() }
-		captchaEdit.onClick { it?.hideKeyboard() }
+		spinner.adapter = SimpleAdapter(this, apiList,
+				android.R.layout.simple_list_item_1, arrayOf("title"), intArrayOf(android.R.id.text1))
+		spinner.onItemSelectedListener = this
+		numberEdit.addTextChangedListener(object : TextWatcher {
+			override fun afterTextChanged(s: Editable?) {}
+			override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+			override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+				if (!s.isNullOrEmpty() && captchaImage.image == null) {
+					refreshCaptcha()
+				}
+			}
+		})
+	}
+
+	override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+	override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+		currentApi = apiList[position]["key"]!!
+		captchaImage.image = null
 		refreshCaptcha()
 	}
 
 	private fun refreshCaptcha() {
-		captchaImage.loadBitmap { CommonApi.getCaptchaNewApi(CommonApi.captchaReferer["2016gkcj"])!! }
+		if (numberEdit.text.isNullOrEmpty()) {
+			return
+		}
+		captchaImage.loadBitmap {
+			if (currentApi.startsWith("2016")) {
+				CommonApi.getCaptchaNewApi(CommonApi.captchaReferer[currentApi])
+			} else {
+				null
+			}
+		}
 	}
 
 	private fun doQueryScore() = doAsync {
 		val result = QueryApi.queryScore(2016, numberEdit.text.toString().toInt(),
 				birthEdit.text.toString(), captchaEdit.text.toString(), null)
 		uiThread {
-			toast(Gson().toJson(result))
 			if (result?.code == 200) {
 				showScoreResultDialog(result.data!!)
 			}
@@ -61,16 +102,46 @@ class MainActivity : AppCompatActivity(), AndroidExtensions {
 		}
 	}
 
+	private fun doQueryAdmission() = doAsync {
+		val result = QueryApi.queryAdmission(2016, numberEdit.text.toString().toInt(),
+				birthEdit.text.toString(), captchaEdit.text.toString(), null)
+		uiThread {
+			if (result?.code == 200) {
+				showAdmissionResultDialog(result.data!!)
+			}
+			refreshCaptcha()
+		}
+	}
+
 	private fun showScoreResultDialog(result: ScoreResult) {
-		var msg = resources.string[R.string.score_result_header]
-				?.format(result.studentName, result.studentNumber, result.issueDate)
-		result.scores
-				.map { resources.string[R.string.score_result_item_format]?.format(it.name, it.point) }
-				.forEach { msg += it }
-		msg += resources.string[R.string.score_result_footer]
 		AlertDialog.Builder(this)
 				.setTitle(R.string.score_result_title)
-				.setMessage(msg)
+				.setMessage(buildString {
+					append(resources.string[R.string.score_result_header]
+							?.format(result.studentName, result.studentNumber, result.issueDate))
+					result.scores
+							.map { resources.string[R.string.score_result_item_format]?.format(it.name, it.point) }
+							.forEach { append(it) }
+					append(resources.string[R.string.score_result_footer])
+				})
+				.setPositiveButton(android.R.string.ok) {_, _ ->}
+				.show()
+	}
+
+	private fun showAdmissionResultDialog(result: AdmissionResult) {
+		AlertDialog.Builder(this)
+				.setTitle(R.string.score_result_title)
+				.setMessage(buildString {
+					append(resources.string[R.string.admission_result_header]
+							?.format(result.studentName, result.studentNumber, result.issueDate))
+					result.admission
+							.map {
+								resources.string[R.string.admission_result_item_format]
+										?.format(it.schoolNumber, it.batch, it.category, it.schoolName)
+							}
+							.forEach { append(it) }
+					append(resources.string[R.string.admission_result_footer])
+				})
 				.setPositiveButton(android.R.string.ok) {_, _ ->}
 				.show()
 	}
